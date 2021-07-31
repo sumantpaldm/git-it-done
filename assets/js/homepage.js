08deb7f30a870e5bc41f2eaf439274fbb5ff49f9
SINGH 1258 <sumantpaldm@gmail.com>
2021-07-31 01:14:31 -0400

    final update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,9 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
-
-
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 
 var formSubmitHandler = function (event) {
@@ -99,8 +97,33 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
